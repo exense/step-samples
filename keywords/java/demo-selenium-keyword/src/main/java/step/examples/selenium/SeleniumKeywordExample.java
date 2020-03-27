0b64fd1adc315afa4a39e462968e3e9a13700abd
Jonathan Rubiero <jonathan.rubiero@exense.ch>
2020-03-27 12:54:57 +0100

    Added Open_chrome_new method

--- keywords/java/demo-selenium-keyword/src/main/java/step/examples/selenium/SeleniumKeywordExample.java
+++ keywords/java/demo-selenium-keyword/src/main/java/step/examples/selenium/SeleniumKeywordExample.java
@@ -21,7 +21,10 @@ import step.handlers.javahandler.AbstractKeyword;
 import step.handlers.javahandler.Keyword;
 
 public class SeleniumKeywordExample extends AbstractKeyword {
-
+	
+	final List<String> defaultOptions = Arrays.asList(new String[] { "disable-infobars", "ignore-certificate-errors" });
+	final List<String> headlessOptions = Arrays.asList(new String[] { "headless", "disable-gpu", "no-sandbox" });
+	
 	@Keyword(name = "Open Chrome")
 	public void openChrome() throws Exception {
 		
@@ -41,6 +44,31 @@ public class SeleniumKeywordExample extends AbstractKeyword {
 		WebDriver driver = new ChromeDriver(options);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		session.put(new DriverWrapper(driver));
+	}
+	
+	@Keyword(name = "Open Chrome New")
+	public void Open_chrome_new() {										
+		ChromeOptions options = new ChromeOptions();
+		options.setAcceptInsecureCerts(true);
+		options.addArguments(defaultOptions);
+		options.setExperimentalOption("w3c", false);
+        
+		if (input.getBoolean("headless", false)) {
+			options.addArguments(headlessOptions);
+		}
+		
+		session.put("enableHarCapture", input.getBoolean("enableHarCapture", false));
+				
+					
+		final WebDriver driver = new ChromeDriver(options);		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+
+		if (input.getBoolean("maximize", false)) {
+			driver.manage().window().maximize();
+		}
+
+		setDriver(driver);
 	}
 
 	@Keyword(name = "Search in google")
@@ -82,6 +110,12 @@ public class SeleniumKeywordExample extends AbstractKeyword {
 			output.appendError("Unable to generate screenshot");
 		}
 	}
+	
+	 private void setDriver(WebDriver driver) {
+		    this.session.put("DriverWrapper", new DriverWrapper(driver));
+		  }
+		  
+
 	// Wrapping the WebDriver instance as it is not implementing the Closeable interface
 	public class DriverWrapper implements Closeable {
 
