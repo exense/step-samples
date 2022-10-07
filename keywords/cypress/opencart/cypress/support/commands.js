e11c11c9333c22e643a9de272e209f7b10dd0ca2
David Stephan <David.Stephan@exense.ch>
2022-10-07 15:46:27 +0200

    SSM-32 adding content for Cypress RPA tutorial

--- keywords/cypress/opencart/cypress/support/commands.js
+++ keywords/cypress/opencart/cypress/support/commands.js
@@ -27,3 +27,22 @@ Cypress.Commands.add('Opencart_PlaceOrder', () => {
   cy.get('#button-confirm').click()
   cy.contains('Your order has been placed!')
 })
+
+Cypress.Commands.add('Opencart_Admin_Login', () => {
+
+
+  cy.visit('https://opencart-prf.exense.ch/admin/')
+  cy.get('#input-username').type('demo')
+  cy.get('#input-password').type('demo')
+  cy.get('button').contains('Login').click()
+  cy.get('a').contains('Catalog').click()
+})
+
+
+Cypress.Commands.add('Opencart_Admin_Update_Product', (productName, quantity) => {
+  cy.get('a').contains('Products').click()
+  cy.get('td').contains(productName).siblings().find('[data-original-title="Edit"]').click()
+  cy.get('a').contains('Data').click()
+  cy.get('#input-quantity').clear().type(quantity)
+  cy.get('[data-original-title="Save"]').click()
+})
