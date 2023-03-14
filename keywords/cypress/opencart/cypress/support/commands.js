919b32623e5e23a333ac4d9b68c72b3db5064cc3
Jérôme Comte <jerome.comte@exense.ch>
2023-03-14 10:03:38 +0100

    Upgrading Cypress samples to Cypress 12

--- keywords/cypress/opencart/cypress/support/commands.js
+++ keywords/cypress/opencart/cypress/support/commands.js
@@ -21,28 +21,10 @@ Cypress.Commands.add('Opencart_PlaceOrder', () => {
   cy.get('#input-payment-country').select('Switzerland')
   cy.get('#input-payment-zone').select('Zürich')
   cy.get('#button-guest').click()
+  cy.wait(1000)
   cy.get('#button-shipping-method').click()
   cy.get('.pull-right > [type="checkbox"]').click()
   cy.get('#button-payment-method').click()
   cy.get('#button-confirm').click()
   cy.contains('Your order has been placed!')
-})
-
-Cypress.Commands.add('Opencart_Admin_Login', () => {
-
-
-  cy.visit('https://opencart-prf.exense.ch/admin/')
-  cy.get('#input-username').type('demo')
-  cy.get('#input-password').type('demo')
-  cy.get('button').contains('Login').click()
-  cy.get('a').contains('Catalog').click()
-})
-
-
-Cypress.Commands.add('Opencart_Admin_Update_Product', (productName, quantity) => {
-  cy.get('a').contains('Products').click()
-  cy.get('td').contains(productName).siblings().find('[data-original-title="Edit"]').click()
-  cy.get('a').contains('Data').click()
-  cy.get('#input-quantity').clear().type(quantity)
-  cy.get('[data-original-title="Save"]').click()
 })
