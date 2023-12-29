8c5ae643bc2ddbf6fbcfcbd91e6d5350607f18de
Jérôme Comte <jerome.comte@exense.ch>
2023-12-29 17:29:46 +0100

    SED-2518 Adding load testing playwright AP sample

---
+++ automation-packages/load-testing-playwright/src/main/java/step/examples/loadtesting/playwright/PlaywrightKeywords.java
@@ -0,0 +1,39 @@
+package step.examples.loadtesting.playwright;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.SelectOption;
+import step.handlers.javahandler.AbstractKeyword;
+import step.handlers.javahandler.Keyword;
+
+public class PlaywrightKeywords extends AbstractKeyword {
+
+    @Keyword(name = "Buy MacBook in OpenCart")
+    public void buyMacBookInOpenCart() throws InterruptedException {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            Page page = browser.newPage();
+            page.navigate("https://opencart-prf.exense.ch/");
+            page.locator("text=MacBook").click();
+            page.locator("text=Add to Cart").click();
+            page.locator("text=1 item").click();
+            page.locator("text=View Cart").click();
+            page.locator("//a[text()='Checkout']").click();
+            page.locator("text=Guest Checkout").click();
+            Thread.sleep(500);
+            page.locator("#button-account").click();
+            page.locator("#input-payment-firstname").type("Gustav");
+            page.locator("#input-payment-lastname").type("Muster");
+            page.locator("#input-payment-email").type("gustav@muster.ch");
+            page.locator("#input-payment-telephone").type("+41777777777");
+            page.locator("#input-payment-address-1").type("Bahnhofstrasse 1");
+            page.locator("#input-payment-city").type("Zurich");
+            page.locator("#input-payment-postcode").type("8001");
+            page.locator("#input-payment-country").selectOption(new SelectOption().setLabel("Switzerland"));
+            page.locator("#input-payment-zone").selectOption(new SelectOption().setLabel("Zürich"));
+            page.locator("#button-guest").click();
+        }
+    }
+}
