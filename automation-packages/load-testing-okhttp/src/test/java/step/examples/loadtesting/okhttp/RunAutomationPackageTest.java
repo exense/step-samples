35df14356a31d5cc4e1d156e39e52324714c23f4
David Stephan <david.stephan@exense.ch>
2024-01-19 16:49:12 +0100

    SED-2518 bumping version in AP samples

--- automation-packages/load-testing-okhttp/src/test/java/step/examples/loadtesting/okhttp/RunAutomationPackageTest.java
+++ automation-packages/load-testing-okhttp/src/test/java/step/examples/loadtesting/okhttp/RunAutomationPackageTest.java
@@ -1,9 +1,10 @@
 package step.examples.loadtesting.okhttp;
 
 import org.junit.runner.RunWith;
-import step.automation.packages.junit.StepAutomationPackageRunner;
+import step.junit.runner.Step;
+
 
 // This test runs the automation package (defined in the resources/ folder)
-@RunWith(StepAutomationPackageRunner.class)
+@RunWith(Step.class)
 public class RunAutomationPackageTest {
 }
--- automation-packages/load-testing-playwright/src/test/java/step/examples/loadtesting/playwright/RunAutomationPackageTest.java
+++ automation-packages/load-testing-playwright/src/test/java/step/examples/loadtesting/playwright/RunAutomationPackageTest.java
@@ -1,9 +1,9 @@
 package step.examples.loadtesting.playwright;
 
 import org.junit.runner.RunWith;
-import step.automation.packages.junit.StepAutomationPackageRunner;
+import step.junit.runner.Step;
 
 // This test runs the automation package (defined in the resources/ folder)
-@RunWith(StepAutomationPackageRunner.class)
+@RunWith(Step.class)
 public class RunAutomationPackageTest {
 }
