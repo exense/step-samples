edad4808f0c33264c177cb0d0ae47c6266b92255
Christoph Langguth <christoph.langguth@exense.ch>
2024-01-19 13:42:27 +0100

    SED-2518 Automation packages fixes and additions (#16)
    * SED-2518 Fixes to AP samples

---
+++ automation-packages/load-testing-okhttp/src/test/java/step/examples/loadtesting/okhttp/RunAutomationPackageTest.java
@@ -0,0 +1,9 @@
+package step.examples.loadtesting.okhttp;
+
+import org.junit.runner.RunWith;
+import step.automation.packages.junit.StepAutomationPackageRunner;
+
+// This test runs the automation package (defined in the resources/ folder)
+@RunWith(StepAutomationPackageRunner.class)
+public class RunAutomationPackageTest {
+}
