85e62d70c991629d7d4d007f0fe5609584c0e25f
Jérôme Comte <jerome.comte@exense.ch>
2024-01-30 14:21:49 +0100

    Fixing selenide sample

--- keywords/java/demo-selenide-keyword/src/main/java/step/examples/selenium/SelenideKeywordExample.java
+++ keywords/java/demo-selenide-keyword/src/main/java/step/examples/selenium/SelenideKeywordExample.java
@@ -1,14 +1,23 @@
 package step.examples.selenium;
 
+import com.codeborne.selenide.Configuration;
 import step.handlers.javahandler.AbstractKeyword;
 import step.handlers.javahandler.Keyword;
 
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SelenideKeywordExample extends AbstractKeyword {
 
     @Keyword(name = "Buy MacBook in OpenCart")
     public void buyMacBookInOpenCart() throws InterruptedException {
-        open("https://opencart-prf.exense.ch/");
+        // Required to run in Step as chrome isn't in the PATH
+        Configuration.browserBinary = "/usr/bin/chromium";
+        try {
+            open("https://opencart-prf.exense.ch/");
+            // ...
+        } finally {
+            closeWebDriver();
+        }
     }
 }
