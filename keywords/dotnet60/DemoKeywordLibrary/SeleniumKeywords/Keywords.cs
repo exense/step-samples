0d46ab449309d1279c78781ba3394bd2b4675f56
Jerome Brongniart <jerome.bronginart@exense.ch>
2024-02-01 09:33:01 +0100

    SED-2411 Migrating dotnet samples to .Net 6 and .Net 8

---
+++ keywords/dotnet60/DemoKeywordLibrary/SeleniumKeywords/Keywords.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using log4net;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Safari;
+using Step.Functions.IO;
+using Step.Grid.IO;
+using Step.Handlers.NetHandler;
+
+namespace SeleniumTest
+{
+    public class Keywords : AbstractKeyword
+    {
+        /**
+         * see the file AssemblyInfo.cs for needed configuration
+         */
+        protected static readonly ILog logger = LogManager.GetLogger(typeof(Keywords));
+
+        [Keyword(name = "Open_Chrome_and_search_in_Google")]
+        public void OpenChromeAndSearchInGoogle()
+        {
+            ChromeDriver driver = CreateChromeDriver();
+            GoogleSearch(driver);
+            driver.Quit();
+        }
+
+
+        [Keyword(name = "Open_Chrome")]
+        public void OpenChrome()
+        {
+            ChromeDriver driver = CreateChromeDriver();
+
+            session.Put("driver", new Wrapper(driver));
+        }
+
+        [Keyword(name = "Open_Safari")]
+        public void OpenSafari()
+        {
+            SafariDriver driver = CreateSafariDriver();
+
+            session.Put("driver", new Wrapper(driver));
+        }
+
+        private SafariDriver CreateSafariDriver()
+        {
+            logger.Info("Creating a Safari Driver");
+
+            SafariOptions options = new SafariOptions();
+            if (properties.ContainsKey("headless") && Boolean.Parse(properties["headless"]))
+            {
+                options.AddAdditionalOption("headless", "true");
+            } else
+            {
+                options.AddAdditionalOption("headless", "true");
+            }
+
+            SafariDriver driver = new SafariDriver(options);
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            return driver;
+        }
+        private ChromeDriver CreateChromeDriver()
+        {
+            logger.Info("Creating a Chrome Driver");
+
+            ChromeOptions options = new ChromeOptions();
+            if (properties.ContainsKey("headless") && Boolean.Parse(properties["headless"]))
+            {
+                options.AddArguments(new string[] { "headless", "disable-infobars", "disable-gpu", "no-sandbox" });
+            }
+
+            ChromeDriver driver = new ChromeDriver(options);
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            return driver;
+        }
+
+        [Keyword(name = "Navigate_to_URL", properties = new string[] { "url_exense" })]
+        public void Navigate()
+        {
+            IWebDriver driver = GetDriver();
+            output.StartMeasure("Navigate");
+            driver.Url = (string)properties["url_exense"];
+            output.StopMeasure();
+        }
+
+        [Keyword(name = "Search_in_google")]
+        public void SearchInGoogle()
+        {
+            IWebDriver driver = GetDriver();
+
+            GoogleSearch(driver);
+        }
+
+        private void GoogleSearch(IWebDriver driver)
+        {
+            if (input["search"] != null)
+            {
+                if (driver == null)
+                {
+                    throw new Exception("Please first execute keyword \"Open Chome\" in order to have a driver available for this keyword");
+                }
+
+                driver.Url = "http://www.google.com";
+                driver.Navigate();
+
+                // Accept cookies and conditions
+                driver.FindElement(By.XPath("//*[@id='L2AGLb']")).Click();
+
+                IWebElement searchInput = driver.FindElement(By.Name("q"));
+
+                String searchString = (string)input["search"];
+                searchInput.SendKeys(searchString + Keys.Enter);
+
+                ICollection<IWebElement> resultHeaders = driver.FindElements(By.XPath("//h3"));
+                foreach (IWebElement result in resultHeaders)
+                {
+                    try
+                    {
+                        output.Add(result.Text, result.FindElement(By.XPath("..")).GetAttribute("href"));
+                    } catch (ArgumentException _) { /*already added to the output*/}
+                }
+                Screenshot ss = ((ITakesScreenshot)driver).GetScreenshot();
+
+                byte[] screenshotAsByteArray = ss.AsByteArray;
+                output.AddAttachment(AttachmentHelper.GenerateAttachmentFromByteArray(screenshotAsByteArray,"screenshot.png"));
+            }
+            else
+            {
+                throw new Exception("Input parameter 'search' not defined");
+            }
+        }
+
+        private IWebDriver GetDriver()
+        {
+            Wrapper wrapper = (Wrapper)session.Get("driver");
+
+            IWebDriver driver = wrapper.driver;
+            return driver;
+        }
+    }
+    public class Wrapper : ICloseable
+    {
+        public IWebDriver driver;
+
+        public Wrapper(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void Close()
+        {
+            if (driver != null)
+            {
+                driver.Quit();
+            }
+        }
+    }
+}
---
+++ keywords/dotnet60/DemoKeywordLibrary/SeleniumKeywords/KeywordsTest.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Step.Functions.IO;
+using Step.Handlers.NetHandler;
+using System;
+using System.Collections.Generic;
+
+namespace SeleniumTest
+{
+    [TestClass]
+    public class KeywordsTests : IDisposable
+    {
+        ExecutionContext Runner;
+        Output Output;
+        public KeywordsTests()
+        {
+            Runner = KeywordRunner.GetExecutionContext(typeof(Keywords));
+        }
+
+        [TestMethod]
+        public void OpenChromeTest()
+        {
+            Output = Runner.Run("Open_Chrome", @"{}", new Dictionary<string, string>() { { "headless", @"false" } });
+            if (Output.error != null)
+            {
+                Console.WriteLine(Output.error.msg);
+            }
+            CheckNoError(Output);
+
+            Output = Runner.Run("Search_in_google", @"{search:'exense'}");
+
+            CheckNoError(Output);
+
+            Assert.AreEqual("https://www.exense.ch/", (string)Output.payload["exense: Home"]);
+        }
+
+        [TestMethod]
+        public void OpenSafariTest()
+        {
+            Output = Runner.Run("Open_Safari", @"{}", new Dictionary<string, string>() { { "headless", @"false" } });
+            CheckNoError(Output);
+
+            Output = Runner.Run("Search_in_google", @"{search:'exense'}");
+            CheckNoError(Output);
+            Assert.AreEqual("https://www.exense.ch/", (string)Output.payload["exense: Home"]);
+        }
+
+        private void CheckNoError(Output ouput)
+        {
+            if (ouput.error != null)
+            {
+                Console.WriteLine(ouput.error.msg);
+            }
+            Assert.IsNull(ouput.error);
+        }
+
+        public void Dispose()
+        {
+            Runner.Close();
+        }
+    }
+}
