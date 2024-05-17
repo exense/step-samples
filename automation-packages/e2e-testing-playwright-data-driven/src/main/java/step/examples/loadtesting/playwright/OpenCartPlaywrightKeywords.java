fe1b89b0c7b70a3c9492c66c7fe4521d28c687d6
David Stephan <david.stephan@exense.ch>
2024-05-17 09:27:55 +0200

    New Playwright sample data driven

---
+++ automation-packages/e2e-testing-playwright-data-driven/src/main/java/step/examples/loadtesting/playwright/OpenCartPlaywrightKeywords.java
@@ -0,0 +1,51 @@
+package step.examples.loadtesting.playwright;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.LoadState;
+import com.microsoft.playwright.options.SelectOption;
+import step.handlers.javahandler.AbstractKeyword;
+import step.handlers.javahandler.Input;
+import step.handlers.javahandler.Keyword;
+
+public class OpenCartPlaywrightKeywords extends AbstractKeyword {
+
+    @Keyword(name = "Buy MacBook in OpenCart")
+    public void buyMacBookInOpenCart(@Input(name = "firstname") String firstname, @Input(name = "lastname") String lastname) throws InterruptedException {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            Page page = browser.newPage();
+            page.navigate("https://opencart-prf.exense.ch/");
+            page.locator("text=MacBook").click();
+            // The previous click loads quite a few resources such as jQuery etc.;
+            // If we don't include this wait, the next click may hang forever.
+            page.waitForLoadState(LoadState.DOMCONTENTLOADED);
+            page.locator("text=Add to Cart").click();
+            page.locator("text=1 item").click();
+            page.locator("text=View Cart").click();
+            page.locator("//a[text()='Checkout']").click();
+            page.locator("text=Guest Checkout").click();
+            page.waitForLoadState(LoadState.DOMCONTENTLOADED);
+            // Another timing issue potentially hanging the script
+            Thread.sleep(500);
+            page.locator("#button-account").click();
+            page.locator("#input-payment-firstname").type(firstname);
+            page.locator("#input-payment-lastname").type(lastname);
+            page.locator("#input-payment-email").type("customer@opencart.demo");
+            page.locator("#input-payment-telephone").type("+41777777777");
+            page.locator("#input-payment-address-1").type("Bahnhofstrasse 1");
+            page.locator("#input-payment-city").type("Zurich");
+            page.locator("#input-payment-postcode").type("8001");
+            page.locator("#input-payment-country").selectOption(new SelectOption().setLabel("Switzerland"));
+            page.locator("#input-payment-zone").selectOption(new SelectOption().setLabel("Zürich"));
+            page.locator("#button-guest").click();
+            page.locator("#button-shipping-method").click();
+            page.locator("//input[@name='agree']").setChecked(true);
+            page.locator("#button-payment-method").click();
+            page.locator("#button-confirm").click();
+            page.locator("text=Your order has been placed!").isVisible();
+        }
+    }
+}
---
+++ automation-packages/e2e-testing-playwright-data-driven/src/main/java/step/examples/loadtesting/playwright/WebmailPlaywrightKeywords.java
@@ -0,0 +1,38 @@
+package step.examples.loadtesting.playwright;
+
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.LoadState;
+import step.handlers.javahandler.AbstractKeyword;
+import step.handlers.javahandler.Keyword;
+
+import java.text.ParseException;
+
+public class WebmailPlaywrightKeywords extends AbstractKeyword {
+
+    @Keyword(name = "Read order confirmation in Webmail")
+    public void readOrderConfirmationInWebmail() throws ParseException {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            Page page = browser.newPage();
+            page.navigate("https://demo-webmail.exense.ch/");
+            page.waitForLoadState(LoadState.DOMCONTENTLOADED);
+            page.locator("#rcmloginuser").fill("customer@opencart.demo");
+            page.locator("#rcmloginpwd").fill("8Fm#%GzdSocv3o");
+            page.locator("#rcmloginsubmit").click();
+
+            page.locator("#messagelist").isVisible();
+            page.locator("#rcmcountdisplay").isVisible();
+
+            // Click the first email sent by "Your Store"
+            page.locator("text=Your Store").nth(0).click();
+
+            FrameLocator iframe = page.frameLocator("#messagecontframe");
+            String title = iframe.locator("//h2[@class='subject']").innerText();
+            String date = iframe.locator("//div[@class='header-summary']//span[@class='text-nowrap']").innerText();
+            //Date dateParsed = new SimpleDateFormat("yyyy-MM-dd HH:mm").parse(date);
+
+            output.add("EmailTitle", title);
+            output.add("EmailDate", date);
+        }
+    }
+}
