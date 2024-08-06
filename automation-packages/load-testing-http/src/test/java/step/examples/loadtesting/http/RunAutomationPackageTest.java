b975cf207767fcfe4577f783411279b9e0fd95ac
Jérôme Comte <jerome.comte@exense.ch>
2024-08-06 13:48:26 +0200

    Adding sample for HTTP KW library

---
+++ automation-packages/load-testing-http/src/test/java/step/examples/loadtesting/http/RunAutomationPackageTest.java
@@ -0,0 +1,10 @@
+package step.examples.loadtesting.http;
+
+import org.junit.runner.RunWith;
+import step.junit.runner.Step;
+
+
+// This test runs the automation package (defined in the resources/ folder)
+@RunWith(Step.class)
+public class RunAutomationPackageTest {
+}
