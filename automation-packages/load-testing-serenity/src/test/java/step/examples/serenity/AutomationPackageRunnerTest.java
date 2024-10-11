bd7d940d5cf89b7649aa048f6fbb8364344f4e39
David Stephan <david.stephan@exense.ch>
2024-10-11 11:10:21 +0200

    Fixing test class name

--- automation-packages/load-testing-serenity/src/test/java/step/examples/serenity/AutomationPackageRunnerTest.java
+++ automation-packages/load-testing-serenity/src/test/java/step/examples/serenity/AutomationPackageRunnerTest.java
@@ -8,5 +8,5 @@ import step.junit.runner.Step;
  * This JUnit runner allows to execute the Step Automation Package locally. It isn't used when running in Step
  */
 @RunWith(Step.class)
-public class AutomationPackageRunner {
+public class AutomationPackageRunnerTest {
 }
