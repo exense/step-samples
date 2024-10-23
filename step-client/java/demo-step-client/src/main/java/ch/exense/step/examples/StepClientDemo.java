54269ec3b2df32fdd43733a395449cebe4d6c1fa
Jérôme Comte <jerome.comte@exense.ch>
2024-10-23 09:56:33 +0200

    SED-3530 Adding simple example of PlanBuilder

--- step-client/java/demo-step-client/src/main/java/ch/exense/step/examples/StepClientDemo.java
+++ step-client/java/demo-step-client/src/main/java/ch/exense/step/examples/StepClientDemo.java
@@ -3,12 +3,19 @@ package ch.exense.step.examples;
 import org.junit.Test;
 import step.client.StepClient;
 import step.core.artefacts.reports.ReportNodeStatus;
+import step.core.plans.Plan;
+import step.core.plans.builder.PlanBuilder;
 import step.core.plans.runner.PlanRunnerResult;
+import step.planbuilder.BaseArtefacts;
+import step.planbuilder.FunctionArtefacts;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeoutException;
+
+import static step.planbuilder.BaseArtefacts.threadGroup;
+import static step.planbuilder.FunctionArtefacts.keyword;
 
 public class StepClientDemo {
 
@@ -29,6 +36,39 @@ public class StepClientDemo {
 
             // Execute the plan
             String executionId = client.getExecutionManager().execute(planId, executionParameters);
+
+            // Wait for the execution to terminate
+            PlanRunnerResult result = client.getExecutionManager().getFuture(executionId).waitForExecutionToTerminate();
+
+            if(result.getResult() == ReportNodeStatus.PASSED) {
+                // Do something
+                System.err.println("The Step execution failed");
+                System.exit(1);
+            }
+        }
+    }
+
+    @Test
+    public static void createAPlanViaPlanBuilderAndExecuteItInStep() throws IOException, TimeoutException, InterruptedException {
+        String controllerUrl = "http://localhost:8080";
+        String user = "admin";
+        String password = "init";
+        // Using credentials for the demo. The use of an API key is recommended for productive use: StepClient(String controllerUrl, String token)
+        try (StepClient client = new StepClient(controllerUrl, user, password)) {
+            Plan plan = PlanBuilder.create()
+                    .startBlock(threadGroup(10, 5))
+                    .add(keyword("MyKeyword"))
+                    .endBlock().build();
+
+            // Set the execution parameters (the drop-downs that are set on the execution
+            // screen in the UI)
+            Map<String, String> executionParameters = new HashMap<>();
+            executionParameters.put("env", "TEST");
+
+            plan = client.getPlans().save(plan);
+
+            // Execute the plan
+            String executionId = client.getExecutionManager().execute(plan.getId().toString(), executionParameters);
 
             // Wait for the execution to terminate
             PlanRunnerResult result = client.getExecutionManager().getFuture(executionId).waitForExecutionToTerminate();
