293faa899190dc7986f18475c8675888dcd4ab77
Jérôme Comte <jerome.comte@exense.ch>
2024-10-31 09:28:00 +0100

    SED-3547 Update samples for Step-26 II (#26)
    * SED-3547 Updating samples for K6 to use the new CLI
    
    * SED-3547 Adding sample for distributed load testing with JMeter

--- automation-packages/load-testing-k6/src/test/java/RunAutomationPackageTest.java
+++
@@ -1,7 +0,0 @@
-import org.junit.runner.RunWith;
-import step.junit.runner.Step;
-
-// This test runs the automation package (defined in the resources/ folder)
-@RunWith(Step.class)
-public class RunAutomationPackageTest {
-}
